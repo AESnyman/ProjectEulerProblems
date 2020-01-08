8d06deae05b7546608486838611f57797a75b449
Anri Goosen <AESnyman@users.noreply.github.com>
2020-01-08 16:13:46 +0200

    Problem 3 - SOLVED

---
+++ 003/Problem003.py
@@ -0,0 +1,20 @@
+import math 
+  
+def maxPrimeFactors (n): 
+    maxPrime = -1
+
+    while n % 2 == 0: 
+        maxPrime = 2
+        n /= 2   
+
+    for i in range(3, int(n**(1/2)) + 1, 2): 
+        while n % i == 0: 
+            maxPrime = i 
+            n = n / i 
+
+    if n > 2: 
+        maxPrime = n 
+      
+    return int(maxPrime) 
+
+print(maxPrimeFactors(600851475143))
